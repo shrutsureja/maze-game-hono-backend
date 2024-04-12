97e3858be95e47afd80bd497706ced5771b1b2b6
shrutsureja <shrutsureja.code@gmail.com>
2024-04-12 06:18:53 +0530

    fix:minor error solved

--- hono-backend/src/index.ts
+++ hono-backend/src/index.ts
@@ -1,33 +1,39 @@
-import { Hono } from 'hono'
-import { cors } from 'hono/cors'
-import { logger } from 'hono/logger';
-import { MazeInputValidationFunction } from '../../Maze/backend/src/utils'
-import { GenerateMaze } from '../../Maze/backend/src/Maze/GenerateMaze'
-import { env } from 'hono/adapter';
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { logger } from "hono/logger";
+import { MazeInputValidationFunction } from "../../Maze/backend/src/validations/MazeInputValidation";
+import { GenerateMaze } from "../../Maze/backend/src/Maze/GenerateMaze";
+import { env } from "hono/adapter";
 
 const app = new Hono();
 
 app.use(logger());
-app.use('/api/*', cors({
-  origin: [ 'https://maze-game-teal.vercel.app' , 'http://localhost:5173']
-}));
+app.use(
+  "/api/*",
+  cors({
+    origin: ["https://maze-game-teal.vercel.app", "http://localhost:5173"],
+  })
+);
 
-app.use('/api/generate', async (c, next)=> {
+app.use("/api/generate", async (c, next) => {
   const body = await c.req.json();
-  const validate = MazeInputValidationFunction(body)
-  if(!validate.success) {
-    c.json({success : false, message: validate});
+  const validate = MazeInputValidationFunction(body);
+  if (!validate.success) {
+    c.json({ success: false, message: validate });
   }
   next();
-})
+});
 
-app.post('/api/generate', async (c) => {
+app.post("/api/generate", async (c) => {
   // data validation
   const body = await c.req.json();
-  const {rows , columns, algorithmName, animation} = body;
+  const { rows, columns, algorithmName, animation } = body;
   const MazeObj = new GenerateMaze(rows, columns);
-  const MazeGrid = MazeObj.generateNewMaze(algorithmName.toLowerCase().split(' ').join(''), animation);
-  return c.json({success : true, data : MazeGrid});
-})
+  const MazeGrid = MazeObj.generateNewMaze(
+    algorithmName.toLowerCase().split(" ").join(""),
+    animation
+  );
+  return c.json({ success: true, data: MazeGrid });
+});
 
-export default app
+export default app;
